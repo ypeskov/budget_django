8b054f895af953a46aaaf0bdcad5dab516a58fa3
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-09-02 22:34:02 +0300

    name of categories fixes

--- categories/models.py
+++ categories/models.py
@@ -14,6 +14,10 @@ class Category(BaseModel):
         parent_name = f' ({self.parent})' if self.parent else ''
         return f'{self.name}{parent_name}'
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
 
 class UserCategory(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_categories')
@@ -25,3 +29,7 @@ class UserCategory(BaseModel):
     def __str__(self):
         parent_name = f' ({self.parent})' if self.parent else ''
         return f'{self.name}{parent_name}'
+
+    class Meta:
+        verbose_name = 'user category'
+        verbose_name_plural = 'user categories'
